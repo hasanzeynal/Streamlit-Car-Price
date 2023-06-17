6d67854720496616c4eb8a2f4f102c94860d2f7f
Hasan Zeynal <122077879+hasanzeynal@users.noreply.github.com>
2023-06-18 01:23:56 +0400

    Delete turboaz-scraping directory

--- turboaz-scraping/src/main/java/az/turbo/main/InternetConnectionException.java
+++
@@ -1,7 +0,0 @@
-package az.turbo.main;
-
-public class InternetConnectionException extends RuntimeException {
-    public InternetConnectionException(String message, Exception e) {
-        super(message, e);
-    }
-}
